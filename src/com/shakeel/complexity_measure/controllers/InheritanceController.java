df29fb6ef15dab54e47a45e0ea203263e99c1589
Sasmini Alawathugoda <47610308+SasminiAlawathugoda@users.noreply.github.com>
2019-08-18 00:43:46 +0530

    Update InheritanceController.java

--- src/com/shakeel/complexity_measure/controllers/InheritanceController.java
+++ src/com/shakeel/complexity_measure/controllers/InheritanceController.java
@@ -2,10 +2,11 @@ package com.shakeel.complexity_measure.controllers;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InheritanceController {
-    public static final String SOURCE_CODE_PATH = "C:/Users/sasmini_112033/Documents/SPM/InheritanceCode.java";
+    public static final String SOURCE_CODE_PATH = "C:/Users/sasmini_112033/Documents/SPM/TBSS-Complexity-Measure/code.java";
     public static final String TYPE = "java";
 
    public void measureInheritanceComplexity() throws Exception{
@@ -13,12 +14,15 @@ public class InheritanceController {
       FileReader file=new FileReader(SOURCE_CODE_PATH);
       BufferedReader reader=new BufferedReader(file);
 
+
        int lines = 0;
        while (reader.readLine() != null)
            lines++;
        reader.close();
 
+
       System.out.println("Number of lines: "+lines);
+
 
       int ciArray[] = printCi(lines, TYPE);
       for(int i=0; i < lines; i++){
@@ -31,9 +35,12 @@ public class InheritanceController {
         FileReader file=new FileReader(SOURCE_CODE_PATH);
         BufferedReader reader=new BufferedReader(file);
 
+
+
         int lines = 0;
         while (reader.readLine() != null)
             lines++;
+
         reader.close();
         return lines;
     }
@@ -87,7 +94,7 @@ public class InheritanceController {
         while(sc1.hasNextLine()){
             String line1 = sc1.nextLine();
 
-            if(line1.contains("class ") || line1.equals("} ") || line1.equals("}") || line1.equals("\\r?\\n") || line1.equals("") || line1.equals("else {") || line1.equals("else{")){
+            if(line1.contains("class ") || line1.contains("} ") || line1.equals("}") || line1.equals(" }") || line1.equals("\\r?\\n") || line1.equals("") || line1.equals("else {") || line1.equals("else{") || line1.equals("// ")){
                 Ci[i] = 0;
             }
             i++;
