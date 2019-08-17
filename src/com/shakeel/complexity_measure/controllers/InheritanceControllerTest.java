ef89da49ae83f6fe14030533b41a1737a47c8ec4
Sasmini Alawathugoda <47610308+SasminiAlawathugoda@users.noreply.github.com>
2019-08-17 17:37:02 +0530

    Inheritance Test class added

---
+++ src/com/shakeel/complexity_measure/controllers/InheritanceControllerTest.java
@@ -0,0 +1,18 @@
+package com.shakeel.complexity_measure.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InheritanceControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void measureInheritanceComplexity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countLines() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void printCi() {
+    }
+}
