b9575123dcba11264f28fda6787145fe7ccb354f
TheTechTitan <48148088+TheTechTitan@users.noreply.github.com>
2019-09-14 15:04:46 +0530

    Size Complexity To return Arraylist
    Size complexity now return arraylist of tokens

--- src/main/java/com/shakeel/complexity_measure/controllers/SizeComplexity.java
+++ src/main/java/com/shakeel/complexity_measure/controllers/SizeComplexity.java
@@ -1,58 +1,27 @@
-package com.shakeel.complexity_measure.controllers;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+package sizeComplexity;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SizeComplexity  {
+public class SizeComplexity8  {
 
-	private int Cs=0;
 	private String[] Arithmetic= {"+","-","*","/","%","=","|",".",">"};
 	private String[] Keywords= {"void","double","int","long","float","String"};
 	private String[] KeywordsOther= {"printf","println","cout","cin","if","for","while","do-while","switch","case","System","out"};
-	private String[] Manipulators= {"endl","\n"};
-	private boolean flag=false;
-	
-	//private String[] Keywords2= {"new","delete","throws"};
+	private String[] Manipulators= {"endl","\n"};	
+	private String[] Keywords2= {"new","delete","throws"};
 	private ArrayList<String> var=new ArrayList<>();
+	private ArrayList<String> tokens;
 	 
-	public void measureSize() throws Exception{
-			
-		FileReader file=new FileReader("C:/Users/Telan Rishan/Downloads/test.txt");
-		BufferedReader reader=new BufferedReader(file);
-		
-		String CurrentLine="",line;
-		
-		while((line = reader.readLine())!= null) {
-			
-			CurrentLine=line;
-			
-			if(flag==true) {
-				if(CurrentLine.indexOf("*/") != -1) {
-					CurrentLine=CurrentLine.substring(CurrentLine.indexOf("*/"),CurrentLine.length());
-					flag=false;		
-				}else {
-					continue;
-				}		
-			}else {
+	public ArrayList measureSize(String str) throws Exception{
 				
-				if(CurrentLine.indexOf("/*") != -1) {	
-					CurrentLine=CurrentLine.substring(0,CurrentLine.indexOf("/*"));
-					flag=true;
-				}	
-			}
-			
-			if(CurrentLine.indexOf("//") != -1) {
-				CurrentLine=CurrentLine.substring(0,CurrentLine.indexOf("//"));
-			}
-			
+		tokens = new ArrayList<>();	
+		String CurrentLine=str;
+								
 			regexChecker(CurrentLine, "dd", 1);
 			detectChar(CurrentLine,Arithmetic,1);
 			detectChar(CurrentLine,Keywords,1);
-			//detectChar(CurrentLine,Keywords2,2);
+			detectChar(CurrentLine,Keywords2,2);
 			detectChar(CurrentLine,Manipulators,1);
 			detectChar(CurrentLine, KeywordsOther, 1);
 			
@@ -60,10 +29,7 @@ public class SizeComplexity  {
 				regexCheckerKeyword(CurrentLine,k,1,false);
 			}
 			
-			System.out.println("");
-		}
-		
-		System.out.println("Result :"+this.Cs);	
+			return tokens;	
 	}
 	
 	public void detectChar(String CurrentLine,String ch[],int val) throws Exception {
@@ -91,21 +57,21 @@ public class SizeComplexity  {
 	        	m = p.matcher(str);
 		}else {
 			p = Pattern.compile(ch);
-	        	m = p.matcher(str);
+	       		m = p.matcher(str);
 		}
 		 
         	while(m.find()) {
-        	 
-        	System.out.print(m.group().replaceAll("[^A-Za-z]", "").substring(0,ch.length()).trim()+", ");
-        	this.Cs+=val;  
-        	  	
+        	 	
+        		//System.out.print(m.group().replaceAll("[^A-Za-z]", "").substring(0,ch.length()).trim()+", ");
+        		tokens.add(m.group().replaceAll("[^A-Za-z]", "").substring(0,ch.length()).trim()+", ");
+      	  	
         		if(keyword==true) {
         		
-        		variable=m.group().substring(ch.length()+1).trim();
+        			variable=m.group().substring(ch.length()+1).trim();
         		
     				if(!var.contains(variable)) {
     					var.add(variable);
-    					//System.out.print("(added variable:"+variable+")");
+					//System.out.print("(added variable:"+variable+")");
     				}
         		}
         	}
@@ -121,15 +87,14 @@ public class SizeComplexity  {
 	        	m = p.matcher(str);
 		}else if(ch=="="){
 			p = Pattern.compile("[\\s\\d\\w][\\=]+");
-	       		m = p.matcher(str);
+	        	m = p.matcher(str);
 		}else{
 			p = Pattern.compile("\\"+ch+"+.");
-	        	m = p.matcher(str);
+	       	 	m = p.matcher(str);
 		}
 		
         	while(m.find()) {	
-			System.out.print(m.group()+", ");
-			this.Cs+=val; 
+        		tokens.add(m.group()+", ");
         	}  
 	}
 }
